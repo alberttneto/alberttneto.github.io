5a76fb860a147379f3cb7b725f310e10a37fa9c4
ALBERTO FERREIRA NETO <alberto.neto@martins.com.br>
2023-06-13 14:42:00 -0300

    ajustes de posicionamento #projetos para tela menores

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 let secaoProjeto = document.getElementById("projetos");
-let projetos = secaoProjeto.querySelectorAll("a");
+let projetos = document.querySelectorAll(".carousel-item");
 
 projetos.forEach(element => {
 
     element.addEventListener("mouseover", function(event){
+        
         let infoProjeto = element.children;
+        let imagem = element.getElementsByTagName("img")[0];
+        imagem.style.filter = "brightness(0.1)";
         infoProjeto[1].style.filter = "brightness(0.1)";
         infoProjeto[0].style.visibility = "visible";
+        console.log(infoProjeto);
     })
 
     element.addEventListener("mouseout", function(event){
         let infoProjeto = element.children;
+        let imagem = element.getElementsByTagName("img")[0];
+        imagem.style.filter = "";
         infoProjeto[1].style.filter = "";  
         infoProjeto[0].style.visibility = "";
     })
